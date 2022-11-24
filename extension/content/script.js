bdb2ab5476d54ba44f9498b2f5824b9732f70366
fabianmerkl <108922830+fabianmerkl@users.noreply.github.com>
2022-11-24 21:36:10 +0100

    Ich bin Numero Uno, und nicht Numero Dos

--- extension/content/script.js
+++ extension/content/script.js
@@ -1,1 +1,14 @@
-let r = document.querySelector(":root");
+
+setTimeout(() => {
+    let r = document.querySelector(":root");
+    const hook = document.querySelector("#owner");
+    const w2gBuddne = document.createElement("button");
+    w2gBuddne.innerHTML = "W2G";
+    w2gBuddne.id = "w2gBuddne";
+    w2gBuddne.addEventListener("onclick", () => {
+        fetch(`localhost:4000/rooms?url=${window.location.href}`)
+    })
+    hook.appendChild(w2gBuddne);
+    console.log("schwanz");
+}, 50)
+
