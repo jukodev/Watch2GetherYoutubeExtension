72fa9cbea008d903f9f6e906bda7e0196af63896
65Juko <thejulianjuko@gmail.com>
2022-11-25 00:18:50 +0100

    fix

--- extension/content/script.js
+++ extension/content/script.js
@@ -1,13 +1,12 @@
-
 setTimeout(() => {
-    let r = document.querySelector(":root");
-    const hook = document.querySelector("#owner");
-    const w2gBuddne = document.createElement("button");
-    w2gBuddne.innerHTML = "W2G";
-    w2gBuddne.id = "w2gBuddne";
-    w2gBuddne.addEventListener("onclick", () => {
-        fetch(`localhost:4000/rooms?url=${window.location.href}`)
-    })
-    hook.appendChild(w2gBuddne);
-}, 500)
-
+	let r = document.querySelector(":root");
+	const hook = document.querySelector("#owner");
+	const w2gBuddne = document.createElement("button");
+	w2gBuddne.innerHTML = "W2G";
+	w2gBuddne.id = "w2gBuddne";
+	hook.appendChild(w2gBuddne);
+	document.querySelector("#w2gBuddne").addEventListener("click", () => {
+		console.log("jooo");
+		fetch(`http://localhost:4000/rooms?url=${window.location.href}`);
+	});
+}, 500);
