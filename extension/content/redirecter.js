373d5df33ae21f717b7d9dcb7d4fbc20278c991a
fabianmerkl <108922830+fabianmerkl@users.noreply.github.com>
2022-11-30 22:25:45 +0100

    mir egal wie schlecht es ist es geht

--- extension/content/redirecter.js
+++ extension/content/redirecter.js
@@ -1,16 +1,52 @@
 document.querySelector("#hagebuddne").addEventListener("click", () => {
-	chrome.tabs.query({ currentWindow: true, active: true }, tabs => {
+		if(!document.querySelector("#token").value || !document.querySelector("#namensdiebstahl").value){
+			setTimeout(() => shake(), 0);
+		}
+		chrome.tabs.query({ currentWindow: true, active: true }, tabs => {
 		var active = tabs[0];
 		let token = document.querySelector("#token").value;
 		let username = document.querySelector("#namensdiebstahl").value;
+		setTimeout(() => spin(), 0); 
 		localStorage.setItem("token", token);
 		localStorage.setItem("username", username);
-		document.querySelector("#buddnewrapper").classList.toggle("obama");
 		chrome.tabs.sendMessage(active.id, {
 			command: token + ":" + username,
 		});
 	});
 });
+
+function spinBot() {
+	document.querySelector("#buddnewrapper").classList.add("obama");
+	setTimeout(() => {
+		document.querySelector("#buddnewrapper").classList.remove("obama");
+	},800);
+}
+function shakeItOff() {
+	document.querySelector("#buddnewrapper").classList.add("shakeitoff");
+	setTimeout(() => {
+		document.querySelector("#buddnewrapper").classList.remove("shakeitoff");
+	},800);
+}
+
+Function.prototype.callAgainOnlyAfter = function(wait) {
+	let nextCallAllowed = 0,
+		debouncedFunc = this;
+ 
+	function debounceCore() {
+	   let now = Date.now();
+	   if (now > nextCallAllowed) {
+		  debouncedFunc.apply(this, arguments);
+	   }
+	   nextCallAllowed = now + wait;
+	}
+	debounceCore.forceNext = function() {
+	   nextCallAllowed = 0;
+	}
+	return debounceCore;
+ };
+
+  const spin = spinBot.callAgainOnlyAfter(800);
+  const shake = shakeItOff.callAgainOnlyAfter(800);
 
 let token = localStorage.getItem("token");
 let username = localStorage.getItem("username");
