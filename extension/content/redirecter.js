9d235f4e97c90996b72bbbf124a928c636915b0f
65Juko <thejulianjuko@gmail.com>
2022-11-30 01:05:32 +0100

    added popup functionality, store token & username

--- extension/content/redirecter.js
+++ extension/content/redirecter.js
@@ -1,14 +1,21 @@
-document.querySelector(".button").addEventListener("click", ()=>{
-    chrome.tabs.query({currentWindow: true, active: true}, (tabs)=>{
-        var active = tabs[0]
-        enabled = !enabled;
-        if(enabled){
-            chrome.tabs.sendMessage(active.id, {command: "enable"})
-        }else{
-            chrome.tabs.sendMessage(active.id, {command: "disable"})
-        }
-        document.querySelector(".text").textContent = enabled? "Enabled" : "Disabled"
-    })
-    
-})
-let enabled = true;
+document.querySelector("#hagebuddne").addEventListener("click", () => {
+	chrome.tabs.query({ currentWindow: true, active: true }, tabs => {
+		var active = tabs[0];
+		let token = document.querySelector("#token").value;
+		let username = document.querySelector("#namensdiebstahl").value;
+		localStorage.setItem("token", token);
+		localStorage.setItem("username", username);
+		chrome.tabs.sendMessage(active.id, {
+			command: token + ":" + username,
+		});
+	});
+});
+
+let token = localStorage.getItem("token");
+let username = localStorage.getItem("username");
+if (token) {
+	document.querySelector("#token").value = token;
+}
+if (username) {
+	document.querySelector("#namensdiebstahl").value = username;
+}
