9a4d06ed8513b181fab228eb921155ec82ecf598
Julian <thejulianjuko@gmail.com>
2024-02-25 15:04:12 +0100

    squad wipe

--- extension/content/script.js
+++ extension/content/script.js
@@ -1,67 +1,14 @@
-let token = localStorage.getItem("token");
-let username = localStorage.getItem("username");
-
-chrome.runtime.onMessage.addListener((msg, sender, response) => {
-	token = msg.command.split(":")[0];
-	username = msg.command.split(":")[1];
-	chrome.storage.sync.set({ username });
-	chrome.storage.sync.set({ arsch: token });
-});
-
 setTimeout(() => {
 	const hook = document.querySelector("#owner");
-	const w2gBuddne = document.createElement("button");
-	const glogge = document.querySelector("#notification-preference-button");
-	toggleMargins();
+	const button = document.createElement("button");
 
-	function callback(mutationList) {
-		mutationList.forEach(() => {
-			toggleMargins();
-		});
-	}
-
-	const observer = new MutationObserver(callback);
-	if (glogge) {
-		observer.observe(glogge, {
-			attributeFilter: ["hidden"],
-			attributeOldValue: true,
-			subtree: true,
-		});
-	}
-	function toggleMargins() {
-		if (glogge) {
-			!glogge.hasAttribute("hidden")
-				? (w2gBuddne.style.marginLeft = "0px")
-				: (w2gBuddne.style.marginLeft = "0.8rem");
-		}
-	}
-
-	w2gBuddne.innerHTML = "W2G";
-	w2gBuddne.id = "w2gBuddne";
+	button.innerHTML = "Auf Insiflix ansehen";
+	button.id = "insi-button";
 	if (hook) {
-		hook.appendChild(w2gBuddne);
-		document.querySelector("#w2gBuddne").addEventListener("click", () => {
-			fetch(`https://miimii-api.obama-prism.com/w2g`, {
-				method: "POST",
-				headers: {
-					"Content-type": "application/json",
-					Authorization: `Bearer ${token}`,
-				},
-				body: JSON.stringify({
-					url: window.location.href,
-					username,
-				}),
-			}).then(res =>
-				res
-					.json()
-					.then(res => {
-						window.open(res.data.url, "_blank").focus();
-					})
-					.catch(error => {
-						88;
-						console.log(error);
-					})
-			);
+		hook.appendChild(button);
+		document.querySelector("#insi-button").addEventListener("click", () => {
+			const url = window.location.href;
+			window.location = `https://insiflix.com/youtuberedirect?url=${url}`;
 		});
 	}
 }, 1000);
